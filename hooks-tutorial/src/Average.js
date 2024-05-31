746f321a7e38554e5dd9eb6877d8911568937204
heejae <gmlwochl123@gmail.com>
2024-05-31 14:25:17 +0900

    useCallback Hook 사용해보기

--- hooks-tutorial/src/Average.js
+++ hooks-tutorial/src/Average.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useCallback } from "react";
 
 const getAverage = (numbers) => {
   console.log("평균값 계산 ... ");
@@ -14,14 +14,17 @@ const Average = () => {
   const [list, setList] = useState([]); // 배열 비구조화 할당
   const [number, setNumber] = useState("");
 
-  const onChange = (e) => {
+  const onChange = useCallback((e) => {
     setNumber(e.target.value);
-  };
-  const onInsert = (e) => {
-    const nextList = list.concat(parseInt(number));
-    setList(nextList);
-    setNumber("");
-  };
+  }, []); // useCallback : 두번째 파라미터가 빈 값일 경우 , 컴포넌트 첫 렌더링 때만 호출
+  const onInsert = useCallback(
+    (e) => {
+      const nextList = list.concat(parseInt(number));
+      setList(nextList);
+      setNumber("");
+    },
+    [number, list]
+  ); // number 혹은 list 값이 변경될 때 마다 호출
 
   const avg = useMemo(() => getAverage(list), [list]); // 두번째 파라미터인 [list]가 변경될 때 getAverage 함수 실행
 
@@ -40,5 +43,17 @@ const Average = () => {
     </div>
   );
 };
+
+// useCallback 예시 (위 아래 같은 코드)
+// useCallback(() => {
+//   console.log("hello world!");
+// }, []);
+
+// useMemo(() => {
+//   const func = () => {
+//     console.log("hello world");
+//   };
+//   return func;
+// }, []);
 
 export default Average;
