f59df5009477255c474cdbe2ac058515518eda63
heejae <gmlwochl123@gmail.com>
2024-06-04 15:22:16 +0900

    할 일 추가하기 구현

--- todolist-app/src/App.js
+++ todolist-app/src/App.js
@@ -1,30 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
 const App = () => {
   const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '안녕하세요 최희재 입니다',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: '스페이스바 스테빌 너무 구려요',
-      checked: true,
-    },
-    {
-      id: 3,
-      text: '소리가 너무 탁해요',
-      checked: false,
-    },
+    // {
+    //   id: 1,
+    //   text: '안녕하세요 최희재 입니다',
+    //   checked: true,
+    // },
+    // {
+    //   id: 2,
+    //   text: '스페이스바 스테빌 너무 구려요',
+    //   checked: true,
+    // },
+    // {
+    //   id: 3,
+    //   text: '소리가 너무 탁해요',
+    //   checked: false,
+    // },
   ]);
+
+  const nextId = useRef(0);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
 
   return (
     <TodoTemplate>
-      <TodoInsert />
+      <TodoInsert onInsert={onInsert} />
       <TodoList todos={todos} />
     </TodoTemplate>
   );
--- todolist-app/src/components/TodoInsert.js
+++ todolist-app/src/components/TodoInsert.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import '../css/TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue('');
+      e.preventDefault(); //브라우저 새로고침방지하기
+    },
+    [onInsert, value],
+  );
   return (
-    <form className="TodoInsert">
-      <input placeholder="일정을 입력하세요" />
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        placeholder="일정을 입력하세요"
+        value={value}
+        onChange={onChange}
+      />
       <button type="submit">
         <MdAdd />
       </button>
@@ -14,3 +32,7 @@ const TodoInsert = () => {
 };
 
 export default TodoInsert;
+
+// 일정항목 추가 기능 구현
+// 인풋상태 관리 및 todos 배열 내 새로운 객체 추가 함수 구현
+// 1. todo insert value 상태관리
