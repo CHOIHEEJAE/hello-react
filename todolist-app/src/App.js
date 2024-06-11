e7bac68dc2bb98821d0802a0434b8024173fc2bb
heejae <gmlwochl123@gmail.com>
2024-06-11 11:14:22 +0900

    useReducer 사용해보기

--- todolist-app/src/App.js
+++ todolist-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useReducer, useState, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
@@ -15,41 +15,53 @@ function createBulkTodos() {
   return array;
 }
 
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case 'INSERT':
+      return todos.concat(action.todo);
+    case 'REEMOVE':
+      return todos.filter((todo) => todo.id !== action.id);
+    case 'TOGGLE':
+      return todos.map((todo) =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+      );
+    default:
+      return todos;
+  }
+}
 const App = () => {
-  const [todos, setTodos] = useState(createBulkTodos);
+  const [todos, dispatch] = useReducer(todoReducer, '', createBulkTodos);
+  // const [todos, setTodos] = useState(createBulkTodos);
   const nextId = useRef(2501);
 
   //일정추가
-  const onInsert = useCallback(
-    (text) => {
-      const todo = {
-        id: nextId.current,
-        text,
-        checked: false,
-      };
-      setTodos((todos) => todos.concat(todo));
-      nextId.current += 1;
-    },
-    [todos],
-  );
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    // setTodos((todos) => todos.concat(todo));
+    dispatch({ type: 'INSERT', todo });
+    nextId.current += 1;
+  }, []);
 
   //일정제거 함수
-  const onRemove = useCallback(
-    (id) => {
-      setTodos((todos) => todos.filter((e) => e.id !== id));
-    },
-    [todos],
-  );
+  const onRemove = useCallback((id) => {
+    dispatch({ type: 'REMOVE', id });
+    // setTodos((todos) => todos.filter((e) => e.id !== id));
+  }, []);
 
   //일정 수정하기
   const onToggle = useCallback((id) => {
-    setTodos(
-      (todos) =>
-        todos.map((e) => (e.id === id ? { ...e, checked: !e.checked } : e)),
-      // 수정하고자 하는 id값의 checked값 반전
-      [todos],
-    );
-  });
+    dispatch({ type: 'TOGGLE', id });
+    // setTodos(
+    //   (todos) =>
+    //     todos.map((e) => (e.id === id ? { ...e, checked: !e.checked } : e)),
+    //   // 수정하고자 하는 id값의 checked값 반전
+    //   [todos],
+    // );
+  }, []);
 
   return (
     <TodoTemplate>
